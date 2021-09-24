8e633f77c08abd0644cfa46346bbf1f33f2545c5
Ghostday <fablekey@gmail.com>
2021-09-25 04:26:51 +1000

    Finalised the logic behind the masteries.

--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,4 @@
 
-const APIKey = 'RGAPI-cebd45d6-1987-402b-a092-6f385a9d9a5d';
+const APIKey = 'RGAPI-c27a6a87-d6d6-44a9-a731-de5788ed8c53';
 
 export default APIKey
--- src/components/Masteries.jsx
+++ src/components/Masteries.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Card } from 'react-bootstrap'
+import championData from '../data/data/en_AU/champion.json'
 
 const cDragonChamps = 'https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/en_au/v1/champions/'
 
@@ -8,23 +9,29 @@ class Masteries extends React.Component {
 
     
     render() {
-        const {
-            championId,
-            championLevel,
-            championPoints,
-            chestGranted,
-        } = this.props.data
 
+        // Defining the variables to be used in the upcoming storage of Data
         const championArr = []
+
+        // Passing the base JSON object to an array for usage of the array Methods
+        const champions = Object.entries(championData.data)
         
         this.props.data.forEach(element => {
-            console.log(element)
-            championArr.push(element.championId)
+            // value[0] seems to only hold the champion name, instead of the key, which is useless
+            // for searching for champions as the API only returns the champion Key, not he name natively
+            let champ = champions.find(value => value[1].key == element.championId)
 
+            // Assigning these values to the object array for ease of manipulation for later
+            champ.championPoints = element.championPoints
+            champ.championLevel = element.championLevel
+            champ.chestGranted = element.chestGranted
             
+            championArr.push(champ)
         });
 
-        console.log(championArr)
+        console.log('Champion Array: ',championArr)
+
+
         return (
             <h1></h1>
         )
